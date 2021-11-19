02f386eda66e1d99bc2e945d415772caac66131f
masahito-nagayama <me18sf67m@gmail.com>
2021-11-19 23:11:27 +0900

    add quiz arrangement

--- app.js
+++ app.js
@@ -1,41 +1,74 @@
-// 問題文を定数で定義する
-const question = 'ごりらになりたいですか？つぎのうちどれ？';
-
-// 選択肢を配列で定義する
-const answers = [
-  'なりたいよ',
-  'なりたくない',
-  'どちらでもいい',
-  'だまれよ'
+// オブジェクトを配列にすることができる。
+const quiz = [
+  {
+    question: '①ごりらになりたいですか？つぎのうちどれ？',
+    answers: [
+      'なりたいよ',
+      'なりたくない',
+      'どちらでもいい',
+      'だまれよ'
+    ],
+    correct: 'なりたいよ'
+  }, {
+    question: '②たぬきになりたいですか？つぎのうちどれ？',
+    answers: [
+      'なりたいよ',
+      'なりたくない',
+      'どちらでもいい',
+      'だまれよ'
+    ],
+    correct: 'だまれよ'
+  }, {
+    question: '③きりんになりたいですか？つぎのうちどれ？',
+    answers: [
+      'なりたいよ',
+      'なりたくない',
+      'どちらでもいい',
+      'だまれよ'
+    ],
+    correct: 'どちらでもいい'
+  }
 ];
 
-// 答え
-const correct = 'なりたいよ';
+// 定数quizの配列の長さを定数quizLengthで定義しておく
+const quizLength =  quiz.length;
+let quizIndex = 0;
+
 // 関数のdocumentでbuttonタグを指定する
 const $button = document.getElementsByTagName('button')
 const buttonLength = $button.length;
 // クイズの問題文、選択肢を定義
 const setupQuiz = () => {
   // js-questionのidのタグを指定し、その中のテキストを定数questionに書き換える
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while(buttonIndex < buttonLength){
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
 setupQuiz();
 
+// if文で正解、不正解のアラートを出す。
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert('正解だ！');
   } else {
     window.alert('ハズレだ！どんまい!');
   }
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+    // 問題数がまだあればここの処理を実行する。
+    setupQuiz();
+  } else {
+    // 問題数がもうない場合はここの処理を実行する。
+    window.alert('お疲れ！頑張ったな！さよなら！');
+  }
 };
 
 // ボタンをクリックしたら正誤判定
-let handleIndex = 0;
+let handlerIndex = 0;
 while (handlerIndex < buttonLength) {
   $button[handlerIndex].addEventListener('click', (e) => {
     clickHandler(e);
